bfccc610b03c8509ab6e5648728f346d575cdaed
Monique Neto <moniqueeneto01@outlook.com>
2022-05-28 17:00:40 -0300

    Terceira Versão

--- exercicios_de_programacao.js
+++ exercicios_de_programacao.js
@@ -1,5 +1,4 @@
-/* 01) Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a
-função) e com ponto de exclamação "!" no final.*/
+/* 01) Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a função) e com ponto de exclamação "!" no final.*/
 
 function cumprimentar(nome){
     return " Olá " + nome + "!";
@@ -15,9 +14,7 @@ function converterIdadeEmAnosParaDias(idade){
 }
 console.log(converterIdadeEmAnosParaDias(25));
 
-/* 03) Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas por um
-funcionário num mês, e o quanto ele recebe por hora. O retorno da função deve ser a string "Salário igual a R$
-X", em que X é o quanto o funcionário ganhou no mês.*/
+/* 03) Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas por um funcionário num mês, e o quanto ele recebe por hora. O retorno da função deve ser a string "Salário igual a R$ X", em que X é o quanto o funcionário ganhou no mês.*/
 
 function calcularSalario(horasTrabalhadas, salarioPago){
     let calcularSalarioPago = horasTrabalhadas*salarioPago;
@@ -26,8 +23,7 @@ function calcularSalario(horasTrabalhadas, salarioPago){
 
 console.log(calcularSalario(150, 40.5))
 
-/* 04) Crie uma função que recebe um número (de 1 a 12 e retorne o mês correspondente como uma string. Por
-    exemplo, se a entrada for 2, a função deverá retornar "fevereiro", pois este é o 2° mês.*/
+/* 04) Crie uma função que recebe um número (de 1 a 12 e retorne o mês correspondente como uma string. Por exemplo, se a entrada for 2, a função deverá retornar "fevereiro", pois este é o 2° mês.*/
 
 function nomeDoMes(mes){
     if(mes == 1)
@@ -68,10 +64,7 @@ function maiorOuIgual(numero1, numero2){
 }
 console.log(maiorOuIgual(1, 12))
 
-/* 06) Escreva uma função que receba um valor booleano ou numérico. Se o parâmetro fornecido for booleano, o
-retorno da função deverá ser o inverso. Por exemplo, se a entrada for false, retornará true. Se o parâmetro for
-numérico, o retorno será o número inverso. Por exemplo, se for fornecido 1, o retorno será 1. Se o parâmetro
-de entrada não for de nenhum dos tipo acima, retorne "booleano ou número esperados, mas o parâmetro é do
+/* 06) Escreva uma função que receba um valor booleano ou numérico. Se o parâmetro fornecido for booleano, o retorno da função deverá ser o inverso. Por exemplo, se a entrada for false, retornará true. Se o parâmetro for numérico, o retorno será o número inverso. Por exemplo, se for fornecido 1, o retorno será 1. Se o parâmetro de entrada não for de nenhum dos tipo acima, retorne "booleano ou número esperados, mas o parâmetro é do
 tipo ...". */
 
 function inversor(valor){
@@ -91,17 +84,6 @@ function inversor(valor){
 console.log(inversor(12))
 
 /* 07) Crie uma função que receba quatro números como parâmetro (numero, minimo, maximo, inclusivo) e retorne se o parâmetro numero (o primeiro) está entre minimo e maximo. Quando o parâmetro inclusivo for true, tenha "entre" como inlusivo, ou seja, considerando se numero é igual a minimo ou a maximo. Caso o parâmetro inclusivo não seja informado, seu valor padrão deverá ser false, portanto, a lógica será exclusiva, não considerando se numero é igual a minimo ou a maximo.*/
-
-/*function estaEntre(numero, minimo, maximo, inclusivo){
-    if(numero > minimo && numero < maximo){
-        if(inclusivo == true){
-
-        }
-    }else if(inclusivo == " "){
-        if()
-    }
-
-}*/
 function estaEntre(minimo, maximo, numero, inclusivo = false) {   
 
     return inclusivo ? numero >= minimo && numero <= maximo : numero > minimo && numero < maximo 
@@ -200,9 +182,11 @@ console.log(filtrarNumeros(["JavaScript", 1, "3", "Web", 20]))
 /* 14) Desenvolva uma função que recebe como parâmetro um objeto e retorne um array de arrays, em que cada elemento é um array formado pelos pares chave/valor que corresponde a um atributo do objeto. Observe os exemplos abaixo para um melhor entendimento:
 */
 
-function objetoParaArray(){
+function objetoParaArray(objeto){
+    return Object.entries(objeto);
 
 }
+console.log(objetoParaArray({nome: "Maria", profissao: "Desenvolvedora de software"}));
 
 
 /* 15) Elabore uma função que receba um array de números e retorne um array que tenha todos os números que são pares e que também tenham índices pares. */
@@ -211,12 +195,190 @@ function receberSomenteOsParesDeIndicesPares(arrayDeNumeros){
     const result = [];
 
     for(let i = 0; i <= arrayDeNumeros.length; i += 2){
-        const numeroPar = arrayDeNumeros[i] % 2 == 0
+        const numeroPar = arrayDeNumeros[i] % 2 == 0;
 
         if(numeroPar)
-        result.push(arrayDeNumeros[i])
+        result.push(arrayDeNumeros[i]);
         
     }
     return result   
 }
 console.log(receberSomenteOsParesDeIndicesPares([10, 70, 22, 43]))
+
+/* 16) A fim de manter o calendário anual ajustado com o movimento de translação da Terra, criou-se os anos bissextos, que têm 366 dias em vez dos 365 presentes nos anos normais.
+Para determinar se um ano é bissexto, é necessário saber se ele é multiplo de 4. Não pode ser múltiplo de 100, exceto se for também múltiplo de 400.
+Com isso em mente, desenvolva uma função que recebe um número correspondente a um ano e retorna se ele é bissexto ou não.*/
+
+function checarAnoBissexto(ano){
+    const divisivelPorQuatro = ano % 4 == 0;
+    const divisivelPorCem = ano % 100 == 0;
+    const divisivelPorQuatrocentos = ano % 400 == 0;
+    return (divisivelPorQuatro && !divisivelPorCem) || divisivelPorQuatrocentos;
+
+
+}
+console.log(checarAnoBissexto(2100));
+
+/* 17) Escreva uma função que receba um array de números e retornará a soma de todos os números desse array. */
+
+function somarNumeros(numeros){
+    let soma = 0;
+    numeros.forEach(numero => soma += numero);
+    return "A soma de todos os números é: "+ soma;
+
+}
+console.log(somarNumeros([10, 10, 10]))
+
+/* 18) Você está trabalhando numa aplicação pessoal de controle de despesas. Na tela principal dessa aplicação, é possível adicionar produtos ou serviços, informando nome, categoria e preço. 
+Uma funcionalidade que você está desenvolvendo no momento é a de somar o total das despesas.
+Crie uma função que receba um array de produtos e retorne o total das despesas.*/
+
+function despesasTotais(itens){
+    var total = 0;
+    for (let item of itens)
+    total += item.preco;
+    return 'O total das despesas é: ' + total;
+    
+
+}
+console.log(despesasTotais([{nome: "Jornal online", categoria: "Informação", preco: 89.99},
+    {nome: "Cinema", categoria: "Entretenimento", preco: 150}]));
+
+/* 19) Numa aplicação Web de investimento financeiro da qual você faz parte da equipe de desenvolvimento, pretende-se adicionar a funcionalidade de calcular a média de um conjunto de números informados pelo usuário.
+Com o intuito de realizar esse cálculo, crie uma função que receba um array com uma quantidade
+indeterminada de números e retorne a média simples desses números.*/
+
+function calcularMedia(numeros){
+    //const quantidadeDeNumeros = numeros.length;
+    let somaTotal = 0;
+    for(numero of numeros) {
+        somaTotal += numero
+    }
+    return 'A média simples é: ' + somaTotal / numeros.length;
+}
+console.log(calcularMedia([0, 10]));
+
+/* 20) Faça uma função que recebe a base e a altura de um triângulo e retorne a área desse triângulo. A precisão deverá ser de duas casas decimais, arredondando se necessário.*/
+
+function areaDoTriangulo(base, altura){
+
+    const calcularAreaDoTriagulo = (base * altura) / 2;
+
+    return 'A área desse triângulo é de: ' + calcularAreaDoTriagulo.toFixed(2);
+
+}
+console.log(areaDoTriangulo(10, 15));
+
+/* 21) Criar uma função que receba um array de números e retorne o menor número desse array*/
+
+function menorNumero(numeros){
+
+    let menor = numeros[0];
+
+    for (let i in numeros){
+        if (numeros[i] < menor){
+            menor = numeros[i];
+            
+        }
+    }
+    return 'O menor número desse array é: ' + menor;
+
+}
+console.log(menorNumero([10, 5, 35, 65]));
+
+/* 22)Desenvolva uma função que receba como parâmetro um número de 1 a 10. Internamente, na função, será gerado um número aleatório de 1 a 10. A função deverá retornar se o parâmetro de entrada foi igual ao número sorteado internamente. Se o valor fornecido foi o sorteado, retorne "Parabéns! O número sorteado foi o X". Se não for igual, retorne "Que pena! O número sorteado foi o X". X é o número que foi sorteado. */
+
+function funcaoDaSorte(numero){
+    let numeroGerado= Math.floor(Math.random()*10 + 1);
+    
+    if(numero == numeroGerado){
+        return "Parabéns! O número sorteado foi o: " + numeroGerado
+    }else{
+        return "Que pena! O número sorteado foi o: " + numeroGerado
+    }
+}
+console.log(funcaoDaSorte(10))
+
+/* 23) Criar uma função que receba uma string como parâmetro e conte quantas palavras tem nela.*/
+function contarPalavras(string){
+
+    let quantidadeDePalavras = string.split(" ").length;
+    return "A quantidade de palavras existentes na frase é: " +quantidadeDePalavras
+
+
+}
+console.log(contarPalavras("Só uma frase"))
+
+/* 24) Desenvolva uma função que recebe um caractere e uma string como parâmetros e retorne a quantidade de vezes que o caractere se repete na string. A função deverá ser case-sensitive, ou seja, irá diferenciar maiúsculas de minúsculas.*/
+
+function contarCaractere(string, caractere){
+    let quantidadeDeCaractere = 0;
+
+    for(i = 0; i < string.length; i++){
+        if(string[i] == caractere){
+            quantidadeDeCaractere++
+        }
+    }
+    return "A quantidade de cracteres contido na frase é: " +quantidadeDeCaractere
+}
+console.log(contarCaractere(" Deise Cristiane Silva", "i"))
+
+/* 25) A fim de criar um mecanismo de busca para sua aplicação, você precisa iniciar criando uma função para identificar palavras semelhantes.
+Escreva uma função que recebe como primeiro parâmetro uma palavra e, como segundo parâmetro, um array de strings. A função deverá filtrar as palavras do array que contêm nelas a string do primeiro parâmetro.*/
+
+function buscarPalavrasSemelhantes(palavra, arrayDeStrings){
+    const result = []
+
+    for(let x of arrayDeStrings){
+        if(x.includes(palavra)){
+            result.push(x);
+        }
+    }
+    return result
+   
+    
+}
+console.log(buscarPalavrasSemelhantes("pro", ["programação", "mobile", "profissional"]))
+
+/* 26) Desenvolva uma função que receba uma string como parâmetro e retorne essa string somente com as consoantes, ou seja, sem as vogais.*/
+
+function removerVogais(string){
+    let consoantes = string.replace(/(a|e|i|o|u)/gi, '');
+    return consoantes;
+
+}
+console.log(removerVogais("Cod3r"))
+
+/* 27) Desenvolva uma função que recebe um objeto como parâmetro e retorne um outro objeto que corresponde ao objeto recebido como parâmetro, porém com as posições das chaves e valores invertidas, conforme exemplo a seguir: */
+
+function inverter(){
+
+}
+
+/* 28) Elabore uma função que recebe dois parâmetros: o primeiro é um array de números e o segundo é um número que especifica uma quantidade de dígitos. Essa função deverá retornar somente aqueles números do array que têm a quantidade de dígitos indicada pelo segundo parâmetro.*/
+
+function filtrarPorQuantidadeDeDigitos(){
+
+}
+
+/* 29) Crie uma função que recebe um array de números e retorna o segundo maior número presente nesse array. */
+
+function segundoMaior(){
+
+}
+
+/* 30) Elabore uma função que recebe um objeto com estudantes e suas notas. As notas de cada estudante estarão num array, conforme exemplo abaixo. Você deverá calcular a média da nota de cada aluno e retornar um objeto com os atributos nome e media, que indica o aluno que teve o melhor desempenho nas notas.*/
+
+function recerberMelhorEstudante(objeto){
+    let mediaAluno = 0;
+    let somaNotas = 0;
+
+    for(let atributo of objeto){
+        somaNotas += atributo.nota;
+        mediaAluno = somaNotas/atributo.nota.length;
+    }
+    return atributo.nome, mediaAluno;
+    
+}
+console.log(recerberMelhorEstudante({ Joao: [8, 7.6, 8.9, 6], Mariana: [9, 6.6, 7.9, 8],
+    Carla: [7, 7, 8, 9] }))
